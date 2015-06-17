65972bf4a24f8a6165170504d0a070a25df43125
edvin wahlberg <kirk@edvins-MacBook-Pro.local>
2015-06-17 17:28:40 +0200

    New java files

---
+++ labb1_1.java
@@ -0,0 +1,30 @@
+package sommar2015;
+//Author: Edvin Wahlberg
+// Labb 1 Uppgfit 1
+public class labb1_1 
+{
+	public static void main(String[] args)
+	{
+	boolean data1 = true;
+	float data2 = 45.8F;
+	int data3 = 29;
+	boolean data4 = data3 < 10;
+	float data5 = 12 / 5;
+	float data6 = data3 * data5;
+	int data7 = 10 % 3;
+	String data8 = "Java programmering";
+	char data9 = 'b';
+	float data10 = (float)data5 / 4;
+	System.out.println ("Variabeln data1: " + data1);
+	System.out.println ("Variabeln data2: " + data2);
+	System.out.println ("Variabeln data3: " + data3);
+	System.out.println ("Variabeln data4: " + data4);
+	System.out.println ("Variabeln data5: " + data5);
+	System.out.println ("Variabeln data6: " + data6);
+	System.out.println ("Variabeln data7: " + data7);
+	System.out.println ("Variabeln data8: " + data8);
+	System.out.println ("Variabeln data9: " + data9);
+	System.out.println ("Variabeln data10: " + data10);
+	}
+	
+}
