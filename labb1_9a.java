278b9d2fc3c370cfd5662d8ae82e17e4ea7e93df
Edvin Wahlberg <edvin.wahlberg@gmail.com>
2015-06-17 19:53:35 +0200

    Update labb1_9a.java

--- labb1_9a.java
+++ labb1_9a.java
@@ -1,10 +1,13 @@
-package sommar2015;
+//Author: Edvin Wahlberg 910721
+//Lab 1 Uppgift 9a
 
 public class labb1_9a 
 {
+//fun main
+//Prints out the sum of all even numbers between 0-200
 	public static void main(String[] args)
 	{
-		int integer = 10;
+		int integer = 200;
 		int acc = 0;
 		while(integer > 0)
 		{
